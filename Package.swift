00db44f0a337cb0618ffca1127542e40f960a341
Wolf McNally <wolf@wolfmcnally.com>
2022-05-18 22:56:32 -0700

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/WolfMcNally/WolfBase",
-            from: "3.3.0"
+            from: "4.0.0"
         ),
     ],
     targets: [
